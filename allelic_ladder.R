0f9e45405f8a4474ce802d7a6b8598474578a490
Alana Alexander <laninsky@gmail.com>
2018-04-14 20:23:36 +1200

    Update allelic_ladder.R

--- allelic_ladder.R
+++ allelic_ladder.R
@@ -1,1 +1,5 @@
-allelic_ladder <- function(working_dir,tabdelim_file,allelic_lader_samples) {
+(working_dir,tabdelim_file,allelic_lader_samples
+ "C:/Users/Alana/Downloads","Sperm_whale_DNA_profile_sample_name_loci.txt","NZsamplenames.txt")
+
+
+allelic_ladder <- function(working_dir,tabdelim_file,allelic_ladder_samples) {
