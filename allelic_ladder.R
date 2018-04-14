108928618121fdf0f932cb327bcfd782a7d65a40
Alana Alexander <laninsky@gmail.com>
2018-04-14 20:53:01 +1200

    Update allelic_ladder.R

--- allelic_ladder.R
+++ allelic_ladder.R
@@ -1,5 +1,14 @@
-(working_dir,tabdelim_file,allelic_lader_samples
- "C:/Users/Alana/Downloads","Sperm_whale_DNA_profile_sample_name_loci.txt","NZsamplenames.txt")
+working_dir <- "C:/Users/Alana/Downloads"
+tabdelim_file <- "Sperm_whale_DNA_profile_sample_name_loci.txt"
+allelic_ladder_samples <- "NZsamplenames.txt"
 
 
 allelic_ladder <- function(working_dir,tabdelim_file,allelic_ladder_samples) {
+ setwd(working_dir)
+ genotypes <- read.table(tabdelim_file,header=FALSE,stringsAsFactors=FALSE,sep="\t")
+ ladder <- read.table(allelic_ladder_samples,header=FALSE,stringsAsFactors=FALSE,sep="\t")
+ allele_list <- NULL
+ 
+ for (i in seq(2,(dim(genotypes)[2]),2)) {
+  
+  
