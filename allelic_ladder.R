0f46cb15d62bed9416dfba4a18d0bc1037b20598
Alana Alexander <laninsky@gmail.com>
2018-04-14 22:44:16 +1200

    Update allelic_ladder.R

--- allelic_ladder.R
+++ allelic_ladder.R
@@ -9,20 +9,8 @@ allelic_ladder <- function(working_dir,tabdelim_file,allelic_ladder_samples) {
  ladder <- read.table(allelic_ladder_samples,header=FALSE,stringsAsFactors=FALSE,sep="\t")
  total_allele_list <- vector("list", (dim(genotypes)[2]-1)/2)
  
-  i <- 2
-  tempallele <- rbind(matrix(genotypes[,i],ncol=1),matrix(genotypes[,(i+1)],ncol=1))
-  allele <- unique(tempallele)
-  allele <- allele[(which(allele[,1]!=0)),1]
-  allele <- sort(allele)
-  allele_counts <- rep(NA,length(allele))
-  ref_allele_counts <- rep(NA,length(allele))
-  for (j in 1:length(allele_counts)) {
-   allele_counts[j] <- sum(genotypes[,i:(i+1)]==allele[j])
-   ref_allele_counts[j] <- sum(genotypes[(which(genotypes[,1] %in% ladder[,1])),i:(i+1)]==allele[j])
-  }
-  total_allele_list[[i/2]] <- rbind(allele,allele_counts, ref_allele_counts)
-  
-  for (i in seq(4,((dim(genotypes)[2])-1),2)) {
+  for (k in (dim(ladder)[1]):1) { 
+   i <- 2
    tempallele <- rbind(matrix(genotypes[,i],ncol=1),matrix(genotypes[,(i+1)],ncol=1))
    allele <- unique(tempallele)
    allele <- allele[(which(allele[,1]!=0)),1]
@@ -34,5 +22,22 @@ allelic_ladder <- function(working_dir,tabdelim_file,allelic_ladder_samples) {
     ref_allele_counts[j] <- sum(genotypes[(which(genotypes[,1] %in% ladder[,1])),i:(i+1)]==allele[j])
    }
    total_allele_list[[i/2]] <- rbind(allele,allele_counts, ref_allele_counts)
-   }
- 
+  
+   for (i in seq(4,((dim(genotypes)[2])-1),2)) {
+    tempallele <- rbind(matrix(genotypes[,i],ncol=1),matrix(genotypes[,(i+1)],ncol=1))
+    allele <- unique(tempallele)
+    allele <- allele[(which(allele[,1]!=0)),1]
+    allele <- sort(allele)
+    allele_counts <- rep(NA,length(allele))
+    ref_allele_counts <- rep(NA,length(allele))
+    for (j in 1:length(allele_counts)) {
+     allele_counts[j] <- sum(genotypes[,i:(i+1)]==allele[j])
+     ref_allele_counts[j] <- sum(genotypes[(which(genotypes[,1] %in% ladder[,1])),i:(i+1)]==allele[j])
+    }
+    total_allele_list[[i/2]] <- rbind(allele,allele_counts, ref_allele_counts)
+    }
+    
+    file <- file("allelic_ladder_by_number_of_ref_samples.txt")
+    writeLines(paste("Using ",k," ref samples (",paste(ladder[,1],collapse=","),"), the following alleles are represented:",sep=""))
+    writeLines(total_allele_list, file)
+    close(file)
